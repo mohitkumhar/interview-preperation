b60830b197cc291fd9a1732d43f3738d72595c3a
Mohit Kumhar <mohitmolela@gmail.com>
2026-07-19 10:45:52 +0530

    sheet: Rotate Array by One

---
+++ sheets/DSA Sheet by Love Babbar/array/7-Rotate-Array-by-One/main.py
@@ -0,0 +1,46 @@
+"""
+https://www.geeksforgeeks.org/problems/cyclically-rotate-an-array-by-one2614/1?utm=codolio
+Rotate Array by One
+Difficulty: Basic
+
+Given an array arr, rotate the array by one position in clockwise direction.
+
+Examples:
+Input: arr[] = [1, 2, 3, 4, 5]
+Output: [5, 1, 2, 3, 4]
+Explanation: If we rotate arr by one position in clockwise 5 come to the front and remaining those are shifted to the end.
+
+Input: arr[] = [9, 8, 7, 6, 4, 2, 1, 3]
+Output: [3, 9, 8, 7, 6, 4, 2, 1]
+Explanation: After rotating clock-wise 3 comes in first position.
+
+Constraints:
+1 ≤ arr.size() ≤ 10^5
+0 ≤ arr[i] ≤ 10^5
+
+Expected Complexities
+Time Complexity: O(n)
+Auxiliary Space: O(1)
+
+"""
+# Approch-1 [Simple Looping]
+
+nums = list(
+    map(int, input("Enter the element of Nums (space seperated): ").split()))
+n = len(nums)
+
+prev = nums[0]
+nums[0] = nums[n - 1]
+
+for i in range(1, n):
+    temp = nums[i]
+    nums[i] = prev
+    prev = temp
+
+print(nums)
+
+
+# Approch-2 [python slicing]
+nums = [1, 2, 3, 4]
+nums[:] = [nums[-1]] + nums[:-1]
+print(nums)
