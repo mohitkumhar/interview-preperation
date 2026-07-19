4746511b5d08540549b868a81d158c2cf2fa8fbb
Mohit Kumhar <mohitmolela@gmail.com>
2026-07-19 14:29:46 +0530

    sheet: minimize the height ii

---
+++ sheets/DSA Sheet by Love Babbar/array/9-Minimize-the-Heights-II/main.cpp
@@ -0,0 +1,55 @@
+/*
+https://www.geeksforgeeks.org/problems/minimize-the-heights3351/1
+Minimize the Heights II
+Difficulty: Medium
+
+Given an array arr[] denoting heights of n towers and a positive integer k.
+
+For each tower, you must perform exactly one of the following operations exactly once.
+
+-Increase the height of the tower by k
+-Decrease the height of the tower by k
+Find out the minimum possible difference between the height of the shortest and tallest towers after you have modified each tower.
+
+Note: It is compulsory to increase or decrease the height by k for each tower. After the operation, the resultant array should not contain any negative integers.
+
+Examples :
+
+Input: k = 2, arr[] = [1, 5, 8, 10]
+Output: 5
+Explanation: The array can be modified as [1+k, 5-k, 8-k, 10-k] = [3, 3, 6, 8]. The difference between the largest and the smallest is 8-3 = 5.
+
+Input: k = 3, arr[] = [3, 9, 12, 16, 20]
+Output: 11
+Explanation: The array can be modified as [3+k, 9+k, 12-k, 16-k, 20-k] = [6, 12, 9, 13, 17]. The difference between the largest and the smallest is 17-6 = 11.
+
+Constraints
+1 ≤ k ≤ 10^7
+1 ≤ n ≤ 10^5
+1 ≤ arr[i] ≤ 10^7
+
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> nums{3, 9, 12, 16, 20};
+    int k = 3;
+    int n = nums.size();
+    int ans = nums[n - 1] - nums[0];
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        int mini = min(nums[0] + k, nums[i + 1] - k);
+        int maxi = max(nums[i] + k, nums[n - 1] - k);
+
+        ans = min(ans, maxi - mini);
+    }
+    cout << ans << endl;
+
+    return 0;
+}
