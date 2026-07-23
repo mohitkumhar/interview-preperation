5f49f45ab862544d1b0731a6a065c6faa1df440f
Mohit Kumhar <mohitmolela@gmail.com>
2026-07-23 22:50:22 +0530

    string: Search Pattern

--- sheets/DSA Sheet by Love Babbar/string/16-Rabin-Karp-Algorithm-For-Pattern-Searching/main.cpp
+++ sheets/DSA Sheet by Love Babbar/string/16-Rabin-Karp-Algorithm-For-Pattern-Searching/main.cpp
@@ -30,3 +30,75 @@ Time Complexity: O(n + m)
 Auxiliary Space: O(m)
 
 */
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s = "geeksforgeeks";
+    string pattern = "geek";
+
+    int n = s.length();
+    int m = pattern.length();
+
+    vector<int> LPS(m, 0);
+    LPS[0] = 0;
+    int length_idx = 0;
+
+    int k = 1;
+
+    while (k < m)
+    {
+        if (pattern[k] == pattern[length_idx])
+        {
+            length_idx++;
+            LPS[k] = length_idx;
+            k++;
+        }
+        else
+        {
+            if (length_idx == 0)
+            {
+                LPS[k] = 0;
+                k++;
+            }
+            else
+                length_idx = LPS[length_idx - 1];
+        }
+    }
+
+    int i = 0;
+    int j = 0;
+
+    vector<int> result;
+
+    while (i < n)
+    {
+        if (s[i] == pattern[j])
+        {
+            i++;
+            j++;
+            if (j == m)
+            {
+                result.push_back(i - m);
+                j = LPS[j - 1];
+            }
+        }
+        else
+        {
+            if (j == 0)
+                i++;
+            else
+                j = LPS[j - 1];
+        }
+    }
+
+    for (int i = 0; i < result.size(); i++)
+        cout << result[i] << " ";
+
+    return 0;
+}
