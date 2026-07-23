4d79aa21b2b9917e68054ab73bf2360bc9f5ac83
Mohit Kumhar <mohitmolela@gmail.com>
2026-07-23 22:09:19 +0530

    string: Search Pattern

--- sheets/DSA Sheet by Love Babbar/string/16-Rabin-Karp-Algorithm-For-Pattern-Searching/main.py
+++ sheets/DSA Sheet by Love Babbar/string/16-Rabin-Karp-Algorithm-For-Pattern-Searching/main.py
@@ -30,8 +30,6 @@ Time Complexity: O(n + m)
 Auxiliary Space: O(m)
 """
 
-
-
 txt = "geeksforgeeks"
 pat = "geek"
 
@@ -56,3 +54,24 @@ while i < m:
         else:
             length_idx = LPS[length_idx - 1]
 
+
+result = []
+
+i = 0
+j = 0
+
+while i < n:
+    if txt[i] == pat[j]:
+        i += 1
+        j += 1
+        if j == m:
+            result.append(i - m)
+            j = LPS[j - 1]
+
+    else:
+        if j != 0:
+            j = LPS[j - 1]
+        else:
+            i += 1
+
+print(result)
