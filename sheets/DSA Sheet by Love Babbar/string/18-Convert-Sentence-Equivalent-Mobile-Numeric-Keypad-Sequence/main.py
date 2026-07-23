bb2df5fb28e2b839154e54b3f056c19dcca37b89
Mohit Kumhar <mohitmolela@gmail.com>
2026-07-23 23:28:09 +0530

    string: Convert Sentence Equivalent Mobile Numeric Keypad Sequence

--- sheets/DSA Sheet by Love Babbar/string/18-Convert-Sentence-Equivalent-Mobile-Numeric-Keypad-Sequence/main.py
+++ sheets/DSA Sheet by Love Babbar/string/18-Convert-Sentence-Equivalent-Mobile-Numeric-Keypad-Sequence/main.py
@@ -18,18 +18,18 @@ Input:
 S = "HEY U"
 Output: 4433999088
 Explanation: For 'H' press '4' two times.
-             For 'E' press '3' two times. For 'Y' press '9' 
+             For 'E' press '3' two times. For 'Y' press '9'
              three times. For white space press '0' one time.
              For 'U' press '8' two times.
- 
 
-Your Task:  
+
+Your Task:
 You dont need to read input or print anything. Complete the function printSequence() which takes a string as input parameter and returns its equivalent mobile numeric keypad sequence as a string.
- 
+
 
 Expected Time Complexity: O(Length of String)
 Expected Auxiliary Space: O(Length of String)
- 
+
 
 Constraints:
 
@@ -39,4 +39,44 @@ Characters of string can be empty space or capital alphabets.
 """
 
 
+def printSequence(S):
 
+    counter = {
+        "A": 2,
+        "B": 22,
+        "C": 222,
+        "D": 3,
+        "E": 33,
+        "F": 333,
+        "G": 4,
+        "H": 44,
+        "I": 444,
+        "J": 5,
+        "K": 55,
+        "L": 555,
+        "M": 6,
+        "N": 66,
+        "O": 666,
+        "P": 7,
+        "Q": 77,
+        "R": 777,
+        "S": 7777,
+        "T": 8,
+        "U": 88,
+        "V": 888,
+        "W": 9,
+        "X": 99,
+        "Y": 999,
+        "Z": 9999,
+        " ": 0,
+    }
+
+    ans = ""
+
+    for char in S:
+        ans += str(counter[char])
+
+    return ans
+
+
+print(printSequence("MOHIT"))
