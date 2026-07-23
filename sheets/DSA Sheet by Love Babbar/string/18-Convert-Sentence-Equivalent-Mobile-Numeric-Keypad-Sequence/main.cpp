b00f1d49a65e37321722dd50c4418e7d1bd35c64
Mohit Kumhar <mohitmolela@gmail.com>
2026-07-23 23:42:38 +0530

    string: changes it formating

--- sheets/DSA Sheet by Love Babbar/string/18-Convert-Sentence-Equivalent-Mobile-Numeric-Keypad-Sequence/main.cpp
+++ sheets/DSA Sheet by Love Babbar/string/18-Convert-Sentence-Equivalent-Mobile-Numeric-Keypad-Sequence/main.cpp
@@ -36,3 +36,54 @@ Constraints:
 1 <= Length of String <= 10^5
 Characters of string can be empty space or capital alphabets.
 */
+
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    map<char, string> map = {
+        {'A', "2"},
+        {'B', "22"},
+        {'C', "222"},
+        {'D', "3"},
+        {'E', "33"},
+        {'F', "333"},
+        {'G', "4"},
+        {'H', "44"},
+        {'I', "444"},
+        {'J', "5"},
+        {'K', "55"},
+        {'L', "555"},
+        {'M', "6"},
+        {'N', "66"},
+        {'O', "666"},
+        {'P', "7"},
+        {'Q', "77"},
+        {'R', "777"},
+        {'S', "7777"},
+        {'T', "8"},
+        {'U', "88"},
+        {'V', "888"},
+        {'W', "9"},
+        {'X', "99"},
+        {'Y', "999"},
+        {'Z', "9999"},
+        {' ', "0"}
+    };
+
+    string s = "MOHIT";
+    
+    string ans = "";
+
+    for (char c: s)
+        ans = ans + map[c];
+
+    cout << ans << endl;
+
+    return 0;
+}
+
