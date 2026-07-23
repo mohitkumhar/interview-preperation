3fe2e5807da4d3435b9b0b8c8a3aab90711a6df8
Mohit Kumhar <mohitmolela@gmail.com>
2026-07-23 22:08:28 +0530

    string: Search Pattern

---
+++ sheets/DSA Sheet by Love Babbar/string/16-Rabin-Karp-Algorithm-For-Pattern-Searching/main.py
@@ -0,0 +1,31 @@
+"""
+https://www.geeksforgeeks.org/problems/search-pattern0205/1
+Search Pattern
+Difficulty: Hard
+
+Given two strings, a text string txt and a pattern string pat, both consisting of lowercase English alphabets. Return the starting indices (0-based) of all the occurrences of the pattern string pat in the text string txt.
+
+Note: Return an empty list in case of no occurrences of pattern.
+
+Examples:
+
+Input: txt = "geeksforgeeks", pat = "geek"
+Output: [0, 8]
+Explanation: The string "geek" occurs twice in txt, one starts at index 0 and the other at index 8.
+
+Input: txt = "abesdu", pat = "edu"
+Output: []
+Explanation: There's no substring "edu" present in txt.
+
+Input: txt = "aabaacaadaabaaba", pat = "aaba"
+Output: [0, 9, 12]
+Explanation:
+
+Constraints:
+1 ≤ txt.size() ≤ 10^5
+1 ≤ pat.size() ≤ txt.size()
+
+Expected Complexities
+Time Complexity: O(n + m)
+Auxiliary Space: O(m)
+"""
