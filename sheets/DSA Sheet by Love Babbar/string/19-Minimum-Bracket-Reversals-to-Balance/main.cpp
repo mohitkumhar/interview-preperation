8d5edac720c7ed2c3cea0375571832b813fdad1e
Mohit Kumhar <mohitmolela@gmail.com>
2026-07-24 12:41:03 +0530

    chore: added PS

---
+++ sheets/DSA Sheet by Love Babbar/string/19-Minimum-Bracket-Reversals-to-Balance/main.cpp
@@ -0,0 +1,27 @@
+/*
+https://www.geeksforgeeks.org/problems/count-the-reversals0401/1
+Minimum Bracket Reversals to Balance
+Difficulty: Medium
+
+Given a string s consisting of only opening and closing curly brackets '{' and '}', find out the minimum reversals required to convert the string into a balanced expression. A reversal means changing '{' to '}' or vice-versa.
+
+Examples:
+
+Input: s = "}{{}}{{{"
+Output: 3
+Explanation: One way to balance is:
+"{{{}}{}}". There is no balanced sequence
+that can be formed in lesser reversals.
+
+Input: s = "{{}{{{}{{}}{{"
+Output: -1
+Explanation: There's no way we can balance this sequence of braces.
+
+Constraints:
+1 ≤ |s| ≤ 10^5
+
+*/
+
+
+
+
