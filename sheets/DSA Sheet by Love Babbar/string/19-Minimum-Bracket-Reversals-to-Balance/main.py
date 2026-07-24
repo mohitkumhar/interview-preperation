a2066a4b20bf53b6f6cab088daf00debadd758b4
Mohit Kumhar <mohitmolela@gmail.com>
2026-07-24 12:40:20 +0530

    string: Minimum Bracket Reversals to Balance soln

--- sheets/DSA Sheet by Love Babbar/string/19-Minimum-Bracket-Reversals-to-Balance/main.py
+++ sheets/DSA Sheet by Love Babbar/string/19-Minimum-Bracket-Reversals-to-Balance/main.py
@@ -20,3 +20,37 @@ Explanation: There's no way we can balance this sequence of braces.
 Constraints:
 1 ≤ |s| ≤ 10^5
 """
+
+s = r"}{{}}{{{"
+
+
+def bracket_reversal_to_balance(s: str) -> int:
+    stack = []
+    opening_brack = 0
+    closing_brack = 0
+
+    for char in s:
+        if char == "}":
+            if stack and stack[-1] == "{":
+                stack.pop()
+                opening_brack -= 1
+            else:
+                if char == "}":
+                    closing_brack += 1
+                stack.append(char)
+
+        else:
+            if char == "}":
+                closing_brack += 1
+            else:
+                opening_brack += 1
+
+            stack.append(char)
+
+    if len(stack) % 2 == 1:
+        return -1
+
+    return ((opening_brack + 1) // 2) + ((closing_brack + 1) // 2)
+
+
+print(bracket_reversal_to_balance(s))
