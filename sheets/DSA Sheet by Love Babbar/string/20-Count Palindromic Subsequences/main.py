37684fd1cafd0e4ebcc4e7dad5c76fe7743e2e83
Mohit Kumhar <mohitmolela@gmail.com>
2026-07-24 13:10:19 +0530

    string: added ps

---
+++ sheets/DSA Sheet by Love Babbar/string/20-Count Palindromic Subsequences/main.py
@@ -0,0 +1,26 @@
+"""
+https://www.geeksforgeeks.org/problems/count-palindromic-subsequences/1
+Count Palindromic Subsequences
+Difficulty: Medium
+
+Given a string s, you have to find the number of palindromic subsequences (need not necessarily be distinct) present in the string s. 
+
+Examples:
+
+Input: s = "abcd"
+Output: 4
+Explanation: palindromic subsequence are : 'a' ,'b', 'c' ,'d'
+
+Input: s = "aab"
+Output: 4
+Explanation: palindromic subsequence are : 'a', 'a', 'b', 'aa'
+
+Input: s = "b"
+Output: 1
+Explanation: palindromic subsequence are : 'b'
+
+Constraints:
+1<= s.size() <=30
+
+"""
+
