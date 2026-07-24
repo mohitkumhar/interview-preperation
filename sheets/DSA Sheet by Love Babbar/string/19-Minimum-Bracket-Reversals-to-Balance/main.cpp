822e7e2c0f1ddfa55ba03068fc05099c7199526d
Mohit Kumhar <mohitmolela@gmail.com>
2026-07-24 13:06:30 +0530

    str: Minimum Bracket Reversals to Balance

--- sheets/DSA Sheet by Love Babbar/string/19-Minimum-Bracket-Reversals-to-Balance/main.cpp
+++ sheets/DSA Sheet by Love Babbar/string/19-Minimum-Bracket-Reversals-to-Balance/main.cpp
@@ -22,6 +22,49 @@ Constraints:
 
 */
 
+#include <iostream>
+#include <string>
+#include <stack>
 
+using namespace std;
 
+int main()
+{
+    string s = "}{{}}{{{";
+    stack<char> stack;
 
+    int open_brack = 0;
+    int close_brack = 0;
+
+    for (char c : s)
+    {
+        if (c == '}')
+        {
+            if (!stack.empty() && stack.top() == '{')
+            {
+                open_brack--;
+                stack.pop();
+            }
+            else
+            {
+                close_brack++;
+                stack.push(c);
+            }
+        }
+        else
+        {
+            if (c == '{')
+                open_brack++;
+            else
+                close_brack--;
+            stack.push(c);
+        }
+    }
+
+    if ((open_brack + close_brack) % 2 == 1)
+        cout << -1 << endl;
+    else
+        cout << (open_brack + 1) / 2 + (close_brack + 1) / 2 << endl;
+
+    return 0;
+}
