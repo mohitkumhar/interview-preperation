57e7af985bb86316998d7638c4a97446211e7485
Mohit Kumhar <mohitmolela@gmail.com>
2026-07-31 23:49:48 +0530

    array and string

---
+++ sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-1 Things to Know in any language/2. What are arrays, strings/main.py
@@ -0,0 +1,4 @@
+"""
+An array is a data structure that stores a collection of elements of the same data type in sequential memory, 
+while a string is a specialized sequence of characters used to represent text
+"""
