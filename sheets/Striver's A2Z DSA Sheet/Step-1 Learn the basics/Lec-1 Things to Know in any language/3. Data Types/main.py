704b7664c1fb72f000bda23b9e8c89aeffcb288a
Mohit Kumhar <mohitmolela@gmail.com>
2026-07-31 23:50:21 +0530

    Data Types

---
+++ sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-1 Things to Know in any language/3. Data Types/main.py
@@ -0,0 +1,9 @@
+"""
+What Are Data Types?
+At the core of any programming language lies the concept of data types a way for the language to understand what kind of value a variable can hold. 
+Data types define not only the nature of the data but also the operations you can perform on it and
+the amount of memory it will consume. If you think of a variable as a box, then the data type is the label on 
+that box telling the computer what’s inside and how to handle it. In some languages (like C++ and Java), you must explicitly declare the data type before using a variable.
+ In others (like Python and JavaScript), the type is determined automatically at runtime based on the assigned value.
+ 
+"""
