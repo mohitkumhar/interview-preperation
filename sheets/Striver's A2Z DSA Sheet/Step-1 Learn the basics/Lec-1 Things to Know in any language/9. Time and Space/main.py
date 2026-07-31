8e9d72467ff463d44c178237c1c0392c5f04fe33
Mohit Kumhar <mohitmolela@gmail.com>
2026-07-31 23:52:25 +0530

    time and space

---
+++ sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-1 Things to Know in any language/9. Time and Space/main.py
@@ -0,0 +1,5 @@
+"""
+Time and space complexity are metrics used in computer science to estimate 
+how much time an algorithm takes to run and how much memory it needs as the input size grows.
+
+"""
