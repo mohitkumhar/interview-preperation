173e15f3958c91825c4a40e6aabba58660231a72
Mohit Kumhar <mohitmolela@gmail.com>
2026-07-31 23:50:40 +0530

    if else statement

---
+++ sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-1 Things to Know in any language/4. If Else statements/main.py
@@ -0,0 +1,4 @@
+"""
+if-else statement is a fundamental programming structure used for decision-making. 
+It executes a specific block of code if a condition is true, and a different block of code if that condition is false.
+"""
