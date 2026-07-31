d9363e5de2e2fbec519103337e3b563cbb785680
Mohit Kumhar <mohitmolela@gmail.com>
2026-07-31 23:51:18 +0530

    for loops

---
+++ sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-1 Things to Know in any language/6. For loops/main.cpp
@@ -0,0 +1,4 @@
+/*
+A for loop is a programming tool used to repeat a block of code a set number of times or go through a list of items one by one.
+It saves time and stops you from writing the same code over and over.
+*/
