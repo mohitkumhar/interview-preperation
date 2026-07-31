1b5d758dc4f1bf59ac6110d5c4a609e62d132aee
Mohit Kumhar <mohitmolela@gmail.com>
2026-07-31 23:51:40 +0530

    while loop

---
+++ sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-1 Things to Know in any language/7. While loops/main.py
@@ -0,0 +1,4 @@
+"""
+A while loop is a programming tool used to run a set of commands over and over again as long as a specific rule stays true. 
+Key items include testing a condition, running the loop body, and updating variables
+"""
