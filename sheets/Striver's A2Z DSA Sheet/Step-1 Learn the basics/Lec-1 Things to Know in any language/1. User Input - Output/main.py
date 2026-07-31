8ea893df388daa71d85dabaa651e06e6023b3761
Mohit Kumhar <mohitmolela@gmail.com>
2026-07-31 23:49:26 +0530

    user input output in python

---
+++ sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-1 Things to Know in any language/1. User Input - Output/main.py
@@ -0,0 +1,21 @@
+"""
+Complete the function printNumber which takes an integer input from the user and 
+prints it on the screen.
+
+Use:-
+for C++ : cout << variable_name;
+for Java : System.out.print();
+for Python : print()
+for Javascript : console.log()
+
+Example 1
+Input(user gives value): 7
+Output: 7
+
+Example 2
+Input(user gives value): -5
+Output: -5
+
+"""
+
+print(123)
