6d9833a156e651a3747de2853bdde7bc4f34671a
Mohit Kumhar <mohitmolela@gmail.com>
2026-07-31 23:53:45 +0530

    added patterns

--- sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-2  Build-up Logical Thinking/1. Patterns/main.py
+++ sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-2  Build-up Logical Thinking/1. Patterns/main.py
@@ -17,3 +17,22 @@ for i in range(n):
 print()
 print()
 print()
+
+"""
+*
+* *
+* * *
+* * * *
+* * * * *
+"""
+n = 5
+
+for i in range(n):
+    for j in range(i + 1):
+        print("*", end=" ")
+    print()
+        
+print()
+print()
+print()
+
