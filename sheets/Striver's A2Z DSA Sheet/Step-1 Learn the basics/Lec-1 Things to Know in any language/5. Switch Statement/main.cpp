fb485487a484e79f21fb291bac6da2e529356bd7
Mohit Kumhar <mohitmolela@gmail.com>
2026-07-31 23:51:05 +0530

    switch statement

---
+++ sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-1 Things to Know in any language/5. Switch Statement/main.cpp
@@ -0,0 +1,3 @@
+/*
+A switch-case statement is a conditional choice tool in programming used to replace long if-else chains. Key components include switch expression, case labels, and break/default
+*/
