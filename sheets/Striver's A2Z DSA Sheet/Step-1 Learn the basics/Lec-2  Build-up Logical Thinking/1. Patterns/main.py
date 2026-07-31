c9f4d536ca1d787c616eb2c1439f26fa1e2269c9
Mohit Kumhar <mohitmolela@gmail.com>
2026-07-31 23:54:09 +0530

    added another star patterns

--- sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-2  Build-up Logical Thinking/1. Patterns/main.py
+++ sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-2  Build-up Logical Thinking/1. Patterns/main.py
@@ -36,3 +36,25 @@ print()
 print()
 print()
 
+
+"""
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+"""
+
+n = 5
+
+for i in range(n):
+    val = 1
+    for j in range(i + 1):
+        print(val, end = " ")
+        val+=1
+    print()
+        
+
+print()
+print()
+print()
