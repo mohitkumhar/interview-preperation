f160853ea181082e7e4808040392206bbab3787e
Mohit Kumhar <mohitmolela@gmail.com>
2026-08-01 23:43:48 +0530

    gcd in python

---
+++ sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-4 Know Basic Maths/4. GCD Or HCF/main.py
@@ -0,0 +1,33 @@
+"""
+GCD of Two Numbers
+
+You are given two integers n1 and n2. You need find the Greatest Common Divisor (GCD) of the two given numbers. Return the GCD of the two numbers.
+
+
+The Greatest Common Divisor (GCD) of two integers is the largest positive integer that divides both of the integers.
+
+
+Example 1
+Input: n1 = 4, n2 = 6
+Output: 2
+
+Explanation: Divisors of n1 = 1, 2, 4, Divisors of n2 = 1, 2, 3, 6
+Greatest Common divisor = 2.
+
+Example 2
+Input: n1 = 9, n2 = 8
+Output: 1
+
+Explanation: Divisors of n1 = 1, 3, 9 Divisors of n2 = 1, 2, 4, 8.
+Greatest Common divisor = 1.
+
+Constraints
+1 <= n1, n2 <= 1000
+"""
+
+import math
+
+a = 4
+b = 6
+
+print(math.gcd(a, b))
