e8d5e31a7dcb15217a2b46b4f042cb10d1ab6501
Mohit Kumhar <mohitmolela@gmail.com>
2026-08-04 23:57:59 +0530

    add implementation to calculate the sum of first N natural numbers using recursion

---
+++ sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-5 Learn Basic Recursion/5. Sum of First N Numbers/main.cpp
@@ -0,0 +1,44 @@
+/*
+Sum of First N Numbers
+
+
+Given an integer N, return the sum of first N natural numbers. Try to solve this using recursion.
+
+
+Example 1
+Input : N = 4
+Output : 10
+Explanation : first four natural numbers are 1, 2, 3, 4.
+Sum is 1 + 2 + 3 + 4 => 10.
+
+Example 2
+Input : N = 2
+Output : 3
+Explanation : first two natural numbers are 1, 2.
+Sum is 1 + 2 => 3.
+
+Constraints
+1 <= N <= 10^3
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int solve(int n)
+{
+    if (n == 0)
+        return 0;
+
+    return n + solve(n - 1);
+}
+
+int main()
+{
+
+    int num = 100;
+    int ans = solve(num);
+    cout << ans;
+
+    return 0;
+}
