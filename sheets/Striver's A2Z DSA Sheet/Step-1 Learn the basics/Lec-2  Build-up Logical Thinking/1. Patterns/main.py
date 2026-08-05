6be7b4b6e3accafcba0e1ff53fe585251a4e3de1
Mohit Kumhar <mohitmolela@gmail.com>
2026-08-05 22:30:31 +0530

    add implementation to print a diamond pattern of asterisks

--- sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-2  Build-up Logical Thinking/1. Patterns/main.py
+++ sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-2  Build-up Logical Thinking/1. Patterns/main.py
@@ -173,3 +173,42 @@ for i in range(n):
 print()
 print()
 print()
+
+
+"""
+        *
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * *
+* * * * * * * * *
+  * * * * * * *
+    * * * * *
+      * * *
+        *
+"""
+n = 5
+for i in range(n):
+    for j in range(n - i):
+        print("", end="  ")
+
+    for k in range(i + 1):
+        print("*", end="  ")
+    print()
+
+stars = 9
+
+for i in range(n):
+    for j in range(i):
+        print("", end="  ")
+     
+    for k in range(stars):
+        print("*", end=" ")
+    print()
+    stars -= 2
+    
+print()
+print()
+print()
+
+
