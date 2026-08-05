c591b8b2e2e55ec820ef53a2c0a800289a5ae914
Mohit Kumhar <mohitmolela@gmail.com>
2026-08-05 22:32:02 +0530

    add implementation to print a sequential number pattern

--- sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-2  Build-up Logical Thinking/1. Patterns/main.py
+++ sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-2  Build-up Logical Thinking/1. Patterns/main.py
@@ -300,3 +300,24 @@ print()
 print()
 print()
 
+
+"""
+1
+2  3
+4  5  6
+7  8  9  10
+11 12 13 14 15
+"""
+
+n = 5
+num = 1
+
+for i in range(n):
+    for j in range(i + 1):
+        print(num, end=" ")
+        num += 1
+    print()
+
+print()
+print()
+print()
