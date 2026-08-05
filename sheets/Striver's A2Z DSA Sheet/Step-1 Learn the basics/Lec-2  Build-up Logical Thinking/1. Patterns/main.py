0347877eca963719446ca56bbf8db0abfdb89e39
Mohit Kumhar <mohitmolela@gmail.com>
2026-08-05 22:29:19 +0530

    add implementation to print a decreasing pattern of numbers

--- sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-2  Build-up Logical Thinking/1. Patterns/main.py
+++ sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-2  Build-up Logical Thinking/1. Patterns/main.py
@@ -103,3 +103,25 @@ for i in range(n):
 print()
 print()
 print()
+
+
+"""
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+"""
+
+n = 5
+
+for i in range(n):
+    val = 1
+    for j in range(n - i):
+        print(val, end=" ")
+        val += 1
+    print()
+
+print()
+print()
+print()
