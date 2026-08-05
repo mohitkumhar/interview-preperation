69c82297037c6362737f7379e5164b278a9ea0cc
Mohit Kumhar <mohitmolela@gmail.com>
2026-08-05 22:35:32 +0530

    add implementation to print a pattern of alphabet letters

--- sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-2  Build-up Logical Thinking/1. Patterns/main.py
+++ sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-2  Build-up Logical Thinking/1. Patterns/main.py
@@ -321,3 +321,26 @@ for i in range(n):
 print()
 print()
 print()
+
+
+"""
+A
+A B
+A B C
+A B C D
+A B C D E
+"""
+
+n = 5
+
+for i in range(n):
+    char = 65
+    for j in range(i + 1):
+        print(chr(char), end=" ")
+        char += 1
+    print()
+    
+print()
+print()
+print()
+
