ccd66b73f482e9b09e6d029afdafa24364d2a4bb
Mohit Kumhar <mohitmolela@gmail.com>
2026-08-05 22:29:35 +0530

    add implementation to print a pyramid pattern of asterisks

--- sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-2  Build-up Logical Thinking/1. Patterns/main.py
+++ sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-2  Build-up Logical Thinking/1. Patterns/main.py
@@ -125,3 +125,26 @@ for i in range(n):
 print()
 print()
 print()
+
+
+"""
+        *
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * *
+"""
+n = 5
+for i in range(n):
+    for j in range(n - i):
+        print("", end=" ")
+
+    for k in range(i + 1):
+        print("*", end=" ")
+    print()
+
+
+print()
+print()
+print()
+
