293916486037689f39317abfdab1e2796f354344
Mohit Kumhar <mohitmolela@gmail.com>
2026-08-05 22:31:04 +0530

    add implementation to print a combined pattern of asterisks

--- sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-2  Build-up Logical Thinking/1. Patterns/main.py
+++ sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-2  Build-up Logical Thinking/1. Patterns/main.py
@@ -212,3 +212,34 @@ print()
 print()
 
 
+
+"""
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+"""
+
+
+n = 5
+
+for i in range(n - 1):
+    for j in range(i + 1):
+        print("*", end=" ")
+    print()
+    
+for i in range(n):
+    for j in range(n - i):
+        print("*", end=" ")
+    print()
+
+
+print()
+print()
+print()
+
