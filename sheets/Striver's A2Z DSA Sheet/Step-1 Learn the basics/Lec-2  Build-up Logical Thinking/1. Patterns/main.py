30cb5b3c34577d57edeff67fcd568a655944a35e
Mohit Kumhar <mohitmolela@gmail.com>
2026-08-06 19:47:16 +0530

    add implementation to print a hollow square pattern using stars

--- sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-2  Build-up Logical Thinking/1. Patterns/main.py
+++ sheets/Striver's A2Z DSA Sheet/Step-1 Learn the basics/Lec-2  Build-up Logical Thinking/1. Patterns/main.py
@@ -563,3 +563,28 @@ print()
 print()
 print()
 
+
+
+"""
+* * * *
+*     *
+*     *
+* * * *
+"""
+
+n = 4
+
+for i in range(n):
+    for j in range(n):
+        if i == 0 or i == n - 1 or j == 0 or j == n - 1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+
+print()
+print()
+print()
+
+
